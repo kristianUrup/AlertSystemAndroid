3c265729a41274c2e5e18d3ffeec82b31e082ceb
kristianUrup <43743914+kristianUrup@users.noreply.github.com>
2020-11-02 10:56:37 +0100

    services for notification hub
    I have made firebase service, and registrationIntentService is also added

--- MachineAlertSubscription/app/src/main/java/com/example/machinealertsubscription/FirebaseService.java
+++ MachineAlertSubscription/app/src/main/java/com/example/machinealertsubscription/FirebaseService.java
@@ -1,4 +1,14 @@
 package com.example.machinealertsubscription;
 
-public class FirebaseService {
+import android.app.IntentService;
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+import androidx.annotation.Nullable;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+
+public class FirebaseService extends FirebaseMessagingService {
+
 }
--- MachineAlertSubscription/app/src/main/java/com/example/machinealertsubscription/RegistrationIntentService.java
+++ MachineAlertSubscription/app/src/main/java/com/example/machinealertsubscription/RegistrationIntentService.java
@@ -1,4 +1,30 @@
 package com.example.machinealertsubscription;
 
-public class RegistrationIntentService {
+import android.app.IntentService;
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+import androidx.annotation.Nullable;
+
+public class RegistrationIntentService extends IntentService {
+
+    private static final String TAG = "RegIntentService";
+    /**
+     * Creates an IntentService.  Invoked by your subclass's constructor.
+     *
+     * @param name Used to name the worker thread, important only for debugging.
+     */
+    public RegistrationIntentService(String name) {
+        super(name);
+    }
+
+    public RegistrationIntentService() {
+        super(TAG);
+    }
+
+    @Override
+    protected void onHandleIntent(@Nullable Intent intent) {
+
+    }
 }
